12998af8c409aa42ae1fbdd27124b860b7139157
Vincent Liao <vincent.liao@protonmail.com>
2023-10-26 15:27:37 +0700

    add: basic table with actual hub event

--- apps/hose/src/index.tsx
+++ apps/hose/src/index.tsx
@@ -3,7 +3,8 @@ import { useEvents } from "@rinza/farcaster-hooks";
 import { z } from "zod";
 import utf8 from "utf8";
 import base64 from "base-64";
-import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export const NotifierEventSchema = z.object({
 	hubEventId: z.number(),
@@ -18,28 +19,24 @@ export type NotifierEventType = z.infer<typeof NotifierEventSchema>;
 
 const App = () => {
 	const [data, isError, isLoading] = useEvents({ shouldListen: true });
-	let lastEvent;
-	if (Array.isArray(data) && data.length > 0) {
-		lastEvent = data[data.length - 1];
-		lastEvent = utf8.decode(base64.decode(lastEvent.raw));
-	}
 
 	return (
-		<div>
-			<p>user!</p>
-			{isLoading && <div>Loading...</div>}
-			{isError && <div>Error</div>}
-			{data && <div>Latest Event: {lastEvent}</div>}
-			<div>
-				<h3>All Events:</h3>
-				<Button>Click me</Button>
-				<ul>
-					{data.map((event, index) => (
-						<li key={event.hubEventId || index}>{event.description}</li>
-					))}
-				</ul>
-			</div>
-		</div>
+		<ScrollArea className="h-[200px] w-[600px] border p-4">
+			<Table className="w-full">
+				<TableBody>
+					{Array.isArray(data) &&
+						data.map((event, index) => (
+							<TableRow key={event.hubEventId || index}>
+								<TableCell className="font-medium">
+									{event.description}
+								</TableCell>
+							</TableRow>
+						))}
+				</TableBody>
+			</Table>
+			{isLoading && <div className="text-center">Loading...</div>}
+			{isError && <div className="text-center text-red-500">Error</div>}
+		</ScrollArea>
 	);
 };
 
