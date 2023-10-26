02d83a9fb2682a935a55a94f414ee11eec10fb11
Vincent Liao <vincent.liao@protonmail.com>
2023-10-26 23:56:56 +0700

    update: split app to App.tsx

--- apps/hose/src/index.tsx
+++ apps/hose/src/index.tsx
@@ -1,69 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { useEvents } from "@rinza/farcaster-hooks";
-import { z } from "zod";
-import utf8 from "utf8";
-import base64 from "base-64";
-import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { Badge } from "@/components/ui/badge";
+import App from "./App";
 import "./globals.css";
-
-const messageTypeMap = new Map([
-	[0, "NONE"],
-	[1, "CAST"],
-	[2, "CAST_REMOVE"],
-	[3, "REACTION"],
-	[4, "REACTION_REMOVE"],
-	[5, "LINK"],
-	[6, "LINK_REMOVE"],
-	[7, "VERIFICATION"],
-	[8, "VERIFICATION_REMOVE"],
-	// [9, "SIGNER_ADD"],
-	// [10, "SIGNER_REMOVE"],
-	[11, "USER_DATA"],
-	[12, "USERNAME_PROOF"],
-]);
-
-export const NotifierEventSchema = z.object({
-	hubEventId: z.number(),
-	hash: z.string(),
-	fid: z.number(),
-	type: z.number(),
-	timestamp: z.number(),
-	description: z.string(),
-	raw: z.string(),
-});
-export type NotifierEventType = z.infer<typeof NotifierEventSchema>;
-
-const App = () => {
-	const [data, isError, isLoading] = useEvents({ shouldListen: true });
-
-	return (
-		<ScrollArea className="h-screen w-[600px] border p-2">
-			<Table className="w-full">
-				<TableBody>
-					{Array.isArray(data) &&
-						data.map((event, index) => (
-							<TableRow key={event.hubEventId || index}>
-								<TableCell>
-									<Badge
-										variant={"secondary"}
-										className="px-0.5 mr-1 font-mono"
-									>
-										{messageTypeMap.get(event.type)}
-									</Badge>
-									{event.description}
-								</TableCell>
-							</TableRow>
-						))}
-				</TableBody>
-			</Table>
-			{isLoading && <div className="text-center">Loading...</div>}
-			{isError && <div className="text-center text-red-500">Error</div>}
-		</ScrollArea>
-	);
-};
 
 // @ts-ignore
 ReactDOM.createRoot(document.getElementById("app")).render(
