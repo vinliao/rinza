6c347370febd86ee6db4718c772982596d25458e
Vincent Liao <vincent.liao@protonmail.com>
2023-11-06 22:09:51 +0700

    update: curry fn on bot-test

--- apps/bot-test/index.ts
+++ apps/bot-test/index.ts
@@ -1,33 +1,31 @@
 import { z } from "zod";
 import { makeBot } from "@rinza/farcaster-bot";
 
-const pictureEcho = async (
-	apiKey: string,
-	signer: string,
-	parentHash?: string,
-) => {
-	const url = "https://api.neynar.com/v2/farcaster/cast";
-	const headers = {
-		"Content-Type": "application/json",
-		api_key: apiKey,
-	};
+const neynarPoster =
+	(apiKey: string, signer: string) =>
+	async (text: string, parentHash?: string) => {
+		const url = "https://api.neynar.com/v2/farcaster/cast";
+		const headers = {
+			"Content-Type": "application/json",
+			api_key: apiKey,
+		};
 
-	const res = await fetch(url, {
-		method: "POST",
-		headers,
-		body: JSON.stringify({
-			text: "echo!",
-			signer_uuid: signer,
-			parent: parentHash,
-		}),
-	});
-	const json = await res.json();
-	return json;
-};
+		const res = await fetch(url, {
+			method: "POST",
+			headers,
+			body: JSON.stringify({
+				text,
+				signer_uuid: signer,
+				parent: parentHash,
+			}),
+		});
+		const json = await res.json();
+		return json;
+	};
 
 const apiKey = z.string().parse(process.env.NEYNAR_API_KEY);
 const pictureSigner = z.string().parse(process.env.PICTURE_NEYNAR_SIGNER_UUID);
-pictureEcho(apiKey, pictureSigner);
+const poster = neynarPoster(apiKey, pictureSigner);
 
 const notifierURL = process.env.IS_DEV
 	? "http://localhost:3000"
@@ -39,5 +37,5 @@ const bot = makeBot({
 	notifierURL,
 });
 bot.onmessage((ctx) => {
-	pictureEcho(apiKey, pictureSigner, ctx.parentHash);
+	poster("echo!", ctx.parentHash);
 });
